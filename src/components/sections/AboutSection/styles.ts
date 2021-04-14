eccbecee0eaa8357c017f69a176fd28a081a2e8c
Johnny <jcarreiropublicidade@gmail.com>
2021-04-14 17:14:42 -0300

    fix: Fix about section layout to mobile

--- src/components/sections/AboutSection/styles.ts
+++ src/components/sections/AboutSection/styles.ts
@@ -20,10 +20,26 @@ export const Container = styled.div`
       flex:1;
       color: ${({theme})=>theme.color.gray_200};
 
-    div{
-      & + div{
-        margin-top: 2rem;
+      div{
+        & + div{
+          margin-top: 2rem;
+        }
       }
     }
+    @media screen and (max-width:1024px){
+      .container{
+        display:flex;
+        flex-direction:column;
+        align-items:flex-start;
+        margin:0 2rem;
+        max-width:1140px;
+
+        & + div{
+          margin-top: 2rem;
+        }
+        .imageContainer{
+          margin-bottom: 2rem;
+        }
+      }
     }
 `;
