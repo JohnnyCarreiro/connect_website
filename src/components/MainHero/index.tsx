7060e62649e0d16f1d83c885fb53a05998acad13
Johnny <jcarreiropublicidade@gmail.com>
2021-04-15 10:44:08 -0300

    feat: Add button to main hero

--- src/components/MainHero/index.tsx
+++ src/components/MainHero/index.tsx
@@ -1,3 +1,4 @@
+import Button from 'components/Button';
 import { ReactNode } from 'react';
 
 import { Container } from './styles';
@@ -8,7 +9,7 @@ interface MainHeroProps {
 
 export const MainHero:React.FC<MainHeroProps> = ({children, ...props}) => {
   return (
-    <Container height="100%" bgImage="/images/main_hero.jpg" >
+    <Container height="100%" bgImage="/images/main_hero.jpg"{...props} >
       <div  className="hero">
       <div className="mainHero">
         <h1>Sua empresa faz negócios online, ou está fora dos negócios?</h1>
@@ -16,6 +17,13 @@ export const MainHero:React.FC<MainHeroProps> = ({children, ...props}) => {
           <h2>
             Com o mundo cada vez mais digitalizado, não ter uma presença digital seria o mesmo que abrir sua empresa em meio ao deserto.
           </h2>
+          <Button
+            isPrimary
+            primaryColor={false}
+            text="Saiba mais"
+            link="#"
+            type="button"
+          />
       </div>
       </div>
       {children && children}
