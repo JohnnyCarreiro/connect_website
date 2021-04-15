7e10ec0dd5e59bbe99c6983d5b68d1c4d2c2ac35
Johnny <jcarreiropublicidade@gmail.com>
2021-04-15 10:55:35 -0300

    feat: Add images and button to projects

--- src/components/sections/ProjectsSection/index.tsx
+++ src/components/sections/ProjectsSection/index.tsx
@@ -1,3 +1,4 @@
+import Button from 'components/Button'
 import { Container } from './styles'
 
 
@@ -10,49 +11,77 @@ export const ProjectsSection:React.FC = () => {
       </div>
       <div className="container1">
         <div className="imageContainer1" >
-          <img src="/images/backInBusiness.svg" alt="Back in Business"/>
+          <img src="/images/starter.svg" alt="Back in Business"/>
         </div>
         <div className="mainContent1">
           <h2>Projeto Inicial</h2>
           <p>
             Precisa começar ou formalizar sua empresa, este projeto existe para que você possa contar com soluções completas para o desenvolvimento e crescimento da sua maca.
           </p>
+          <Button
+            isPrimary={false}
+            primaryColor={false}
+            text="Saiba mais"
+            link="#"
+            type="button"
+          />
         </div>
       </div>
 
       <div className="container2">
         <div className="imageContainer2" >
-          <img src="/images/backInBusiness.svg" alt="Back in Business"/>
+          <img src="/images/small.svg" alt="Back in Business"/>
         </div>
         <div className="mainContent2">
           <h2>Pequenos projetos</h2>
           <p>
             Esses projetos destinam-se a aqueles clientes que desejam possuir sua própria plataforma, seus dados e veem a necessidade no futuro de poder personalizar toda sua experiência de oferta de produtos e serviços.
           </p>
+          <Button
+            isPrimary={false}
+            primaryColor={false}
+            text="Saiba mais"
+            link="#"
+            type="button"
+          />
         </div>
       </div>
 
       <div className="container3">
         <div className="imageContainer3" >
-          <img src="/images/backInBusiness.svg" alt="Back in Business"/>
+          <img src="/images/medium.svg" alt="Back in Business"/>
         </div>
         <div className="mainContent3">
           <h2>Médios projetos</h2>
           <p>
             À medida que as vendas aumentam e o pessoal aumenta, surge uma maior necessidade de automação. Os detalhes se tornam mais importantes à medida que o número de visitantes aumenta, e pequenas coisas podem fazer uma grande diferença.
           </p>
+          <Button
+            isPrimary={false}
+            primaryColor={false}
+            text="Saiba mais"
+            link="#"
+            type="button"
+          />
         </div>
       </div>
 
       <div className="container4">
         <div className="imageContainer4" >
-          <img src="/images/backInBusiness.svg" alt="Back in Business"/>
+          <img src="/images/large.svg" alt="Back in Business"/>
         </div>
         <div className="mainContent4">
           <h2>Grandes projetos</h2>
           <p>
             Quando uma empresa está aqui, percebe que, para obter o melhor resultado possível para o seu negócio, a equipe precisa das melhores ferramentas possíveis e essas ferramentas muitas vezes precisam ser personalizadas.
           </p>
+          <Button
+            isPrimary={false}
+            primaryColor={false}
+            text="Saiba mais"
+            link="#"
+            type="button"
+          />
         </div>
       </div>
     </Container>
