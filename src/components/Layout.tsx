81b446499da2263bd832b7da48475fad68cdd0b2
Johnny <jcarreiropublicidade@gmail.com>
2021-04-15 15:40:07 -0300

    fix: Fix About section bg color and svg image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react'
 import Head from 'next/head'
-import { Header } from './Header'
 import Navbar from './Navbar'
 
 type Props = {
--- src/components/sections/AboutSection/styles.ts
+++ src/components/sections/AboutSection/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    /* background: ${({theme})=>theme.color.gray_300}; */
+    background: ${({theme})=>theme.color.gray_500};
     padding: 4rem 0;
 
   .container{
