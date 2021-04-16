bb491eddb0a9cb99f0cb844a5b3f42f91385100c
Johnny <jcarreiropublicidade@gmail.com>
2021-04-16 11:41:07 -0300

    feat: Create services section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MainHero } from 'components/MainHero'
 import { AboutSection } from 'components/sections/AboutSection'
 import { ProjectsSection } from 'components/sections/ProjectsSection'
+import { ServicesSection } from 'components/sections/ServicesSection'
 import { Layout } from '../components/Layout'
 
 const IndexPage = () => (
@@ -11,6 +12,9 @@ const IndexPage = () => (
     </section>
     <section>
       <ProjectsSection/>
+    </section>
+    <section>
+      <ServicesSection/>
     </section>
     <h1>Hello World 👋</h1>
   </Layout>
